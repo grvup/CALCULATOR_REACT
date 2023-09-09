981bb6224d3a0236ab4ae5043991772c26f41d69
Gaurav <upadhyaygaurav450@gmail.com>
2023-09-09 23:09:07 +0530

    third commit

--- src/Body.js
+++ src/Body.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import logo3 from './backspace-icon.png';
 export default function Body() {
 
     const [count,setCount] = React.useState("0");
-   const [count1,setCount1] = React.useState("");
+    const [count1,setCount1] = React.useState("");
     const [count2,setCount2] = React.useState("");
     const [sign,setSign] = React.useState("");
     const [cnt,setCnt] = React.useState(0);
-   const [flag,setFlag] = React.useState(0);
+    const [flag,setFlag] = React.useState(0);
+    const [dotcounter,setDotcounter] = React.useState(0)
+   // const [negcounter,setNegcounter] = React.useState(1)
     let f=0
     function getValue(y)
     {
@@ -18,15 +20,14 @@ export default function Body() {
         }
         if((y===' + ' || y===' - ' || y===' * ' || y===' / ')&& sign==='')
         {
-            
             setCount2(x=>x+count)
            // setCount1(x=>x+count)
            // setCount2(x=>x+y)
             setSign(x=>x+y)
             setCnt(cnt+1)
             setFlag(1);
-            f=1
-           
+            setDotcounter(0)
+            f=1 
         }
         //console.log(f)
         if(f===1 )
@@ -49,6 +50,7 @@ export default function Body() {
                     setCnt(cnt+1)
                     setCount2(x=>result)
                     setCount(x=>result)
+                    setDotcounter(0)
                     //setCount("0")
                 }
                 if(sign===' - ' &&flag===0)
@@ -59,6 +61,7 @@ export default function Body() {
                     setCount2(x=>result)
                     setCount(x=>result)
                     setCnt(cnt+1)
+                    setDotcounter(0)
                    // setCount("0")
                 }
                 if(sign===' * '  &&flag===0)
@@ -69,6 +72,7 @@ export default function Body() {
                     setCount2(x=>result)
                     setCount(x=>result)
                     setCnt(cnt+1)
+                    setDotcounter(0)
                     //setCount("0")
 
                 }
@@ -80,6 +84,7 @@ export default function Body() {
                     setCount2(x=>result)
                     setCount(x=>result)
                     setCnt(cnt+1)
+                    setDotcounter(0)
                    // setCount("0")
                 }
                 setFlag(1);
@@ -94,14 +99,30 @@ export default function Body() {
                 {
                     setCount("")
                 }
-                setCount(x=>x+y)  
-                setCount1(x=>x+y)
+                if(y==='.')
+                {
+                    if(dotcounter===0)
+                    {
+                        setCount(x=>x+y)  
+                        setCount1(x=>x+y)
+                        setDotcounter(dotcounter+1)
+                    }
+                    else
+                    {
+                        setDotcounter(dotcounter+1)
+                    }
+                    
+                }
+                else if(y!=='.')
+                {
+                    setCount(x=>x+y)  
+                    setCount1(x=>x+y)
+                }
                 setCnt(cnt+1)
                 setFlag(0)  
             }
             
         }
-       
 
     }  
     function getOutput()
@@ -145,15 +166,49 @@ export default function Body() {
         setSign("")
     }
 
+    function changeSign()
+    {
+       
+            const num1 = parseFloat(count);
+            const num2 = parseFloat(count1);
+            if(num1>0)
+            {
+                setCount(x=>'-'+x)
+            }
+            else if(num1<0)
+            {
+                const substringToRemove = "-";
+                const resultString = count.replace(substringToRemove, '');
+                const resultString1 = count1.replace(substringToRemove, '');
+                setCount(resultString)
+                setCount1(resultString1)
+            }
+            if(num2>0)
+            {
+                setCount1(x=>'-'+x)
+            }
+            else if(num2<0)
+            {
+                const substringToRemove = "-";
+                const resultString = count.replace(substringToRemove, '');
+                const resultString1 = count1.replace(substringToRemove, '');
+                setCount(resultString)
+                setCount1(resultString1)
+            }
+   
 
+    }
     function clear(){
         setCount("0")
-       setCount1("")
+        setCount1("")
         setCount2("")
         setSign("")
-       setCnt(0)
-      setFlag(0)
+        setCnt(0)
+        setFlag(0)
+        setDotcounter(0)
     }
+
+  
 
    // console.log(sign)
     // console.log(cnt)
@@ -189,12 +244,15 @@ export default function Body() {
                 <div  className="grid-item9" onClick={()=>getValue('4')}>4</div>
                 <div  className="grid-item10" onClick={()=>getValue('5')}>5</div>
                 <div  className="grid-item11" onClick={()=>getValue('6')}>6</div>
-                <div  className="grid-item12">B</div>
+                <div  className="grid-item12">
+                    <img src={logo3} className="img" alt=""/>
+                </div>
                 <div  className="grid-item13"  onClick={()=>getValue('1')}>1</div>
                 <div  className="grid-item14" onClick={()=>getValue('2')}>2</div>
                 <div  className="grid-item15" onClick={()=>getValue('3')}>3</div>
                 <div  className="grid-item16" onClick={getOutput}>=</div>
                 <div  className="grid-item17" onClick={()=>getValue('0')}>0</div>
+                <div  className="grid-item18" onClick={changeSign}>&#8722; </div>
                 <div  className="grid-item19" onClick={()=>getValue('.')}>.</div> 
             </div>
         </div>
